d870eea10dfa6f9d3d5fc591be8b7c8174d01990
rudakovic <filip.rudakovic@gmail.com>
2025-02-22 18:58:44 +0100

    adding custom belvi api call to get beer to present it in beer section
    Signed-off-by: rudakovic <filip.rudakovic@gmail.com>

--- wp-content/plugins/belvi-plugin/assets/belvi-script.js
+++ wp-content/plugins/belvi-plugin/assets/belvi-script.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
+});
+
+function openBeer(e, id) {
+    e.preventDefault();
+
     const apiUrl = belviPlugin.api_url;
     const nonce = belviPlugin.nonce;
 
-    function openBeer(e, id) {
-        e.preventDefault();
+    const postUrl = `${apiUrl}${id}`;
 
-        const postUrl = `${apiUrl}${id}`;
-
-        fetch(postUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-WP-Nonce': nonce
-            }
+    fetch(postUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-WP-Nonce': nonce
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
-});
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
