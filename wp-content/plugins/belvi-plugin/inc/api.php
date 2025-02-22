d001226452f3c827489ce3e2846b3229e1aa8c7a
rudakovic <filip.rudakovic@gmail.com>
2025-02-23 00:21:41 +0100

    add all fields to api response
    Signed-off-by: rudakovic <filip.rudakovic@gmail.com>

--- wp-content/plugins/belvi-plugin/inc/api.php
+++ wp-content/plugins/belvi-plugin/inc/api.php
@@ -15,7 +15,7 @@ function belvi_register_api_routes() {
 add_action('rest_api_init', 'belvi_register_api_routes');
 
 function belvi_get_custom_posts($request) {
-	$post_id = $request->get_param('id');
+	$post_id = (int) $request['id'];
 
 	$args = [
 		'post_type'      => 'beer',
@@ -27,26 +27,36 @@ function belvi_get_custom_posts($request) {
 	}
 
 	$query = new WP_Query($args);
-	$posts = [];
+	$post = [];
 
 	if ($query->have_posts()) {
 		while ($query->have_posts()) {
 			$query->the_post();
 			$brewery = get_field('brewery');
+			$beer_style = get_field('beer_style');
+			$brewery_icon = get_field('brewery_icon');
+			$abv = get_field('abv');
+			$ibu = get_field('ibu');
+			$featured_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
 
-			$posts[] = [
+			$post = [
 				'id'        => get_the_ID(),
 				'title'     => get_the_title(),
 				'content'   => get_the_content(),
+				'image'     => $featured_image,
 				'brewery'   => $brewery,
+				'beer_style'   => $beer_style,
+				'brewery_icon'   => $brewery_icon,
+				'abv'   => $abv,
+				'ibu'   => $ibu,
 			];
 		}
 		wp_reset_postdata();
 	}
 
-	if (empty($posts)) {
+	if (empty($post)) {
 		return new WP_Error('no_post', 'No post found', ['status' => 404]);
 	}
 
-	return rest_ensure_response($posts);
+	return rest_ensure_response($post);
 }
