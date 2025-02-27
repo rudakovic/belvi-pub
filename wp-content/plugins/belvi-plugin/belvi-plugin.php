5165fd0b9124f6b44ee644b8db06f2ced6d47f6b
rudakovic <filip.rudakovic@gmail.com>
2025-02-27 19:24:46 +0100

    prevent to go on any page except homepage
    Signed-off-by: rudakovic <filip.rudakovic@gmail.com>

--- wp-content/plugins/belvi-plugin/belvi-plugin.php
+++ wp-content/plugins/belvi-plugin/belvi-plugin.php
@@ -19,3 +19,23 @@ function belvi_enqueue_assets() {
 add_action('wp_enqueue_scripts', 'belvi_enqueue_assets');
 
 include_once plugin_dir_path(__FILE__) . 'inc/api.php';
+
+function restrict_all_except_one() {
+	// Allow only this post/page (set by ID)
+	$allowed_post_id = 28;
+
+	// Get current post ID
+	if (is_single() || is_page()) {
+		$current_post_id = get_queried_object_id();
+
+		if ($current_post_id != $allowed_post_id) {
+			global $wp_query;
+			$wp_query->set_404();
+			status_header(404);
+			nocache_headers();
+			include(get_query_template('404'));
+			exit;
+		}
+	}
+}
+add_action('template_redirect', 'restrict_all_except_one');
